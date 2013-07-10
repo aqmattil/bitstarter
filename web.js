e53d829b1e1aff29fddf86db483537e09ef3e02c
Ubuntu <ubuntu@ip-10-227-163-199.eu-west-1.compute.internal>
2013-07-10 12:28:36 +0300

    ...4

--- web.js
+++ web.js
@@ -2,6 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+infile = "index.html";
+var text = fs.readFileync(infile);
+consol.log(text);
+
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });

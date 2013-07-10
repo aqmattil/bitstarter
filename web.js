23b63e38a73a81bf6b4309fdeb2c8d97810b54b0
Ubuntu <ubuntu@ip-10-227-163-199.eu-west-1.compute.internal>
2013-07-10 21:58:14 +0300

    ..

--- web.js
+++ web.js
@@ -5,7 +5,8 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 
 app.get('/', function(request, response) {
-  var text = fs.readFile("index.html", encoding="utf-8");
+//  var text = fs.readFile("index.html", encoding="utf-8");
+  var text = "Test";
   response.send(text);
 
 });

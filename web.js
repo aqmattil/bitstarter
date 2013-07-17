664d208b69f092936ca231347cfeb83d8ccd6a99
Ubuntu <ubuntu@ip-10-227-163-199.eu-west-1.compute.internal>
2013-07-17 13:07:19 +0300

    stylesheets added

--- web.js
+++ web.js
@@ -1,17 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
-var fs = require('fs');
-
 app.get('/', function(request, response) {
-  var text = fs.readFileSync("index.html", encoding="utf-8");
-//  var text = "Test";
-  response.send(text);
-
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8082;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
